b626cf78406d327d9702ba75f2e1c53a4b4c650d
Alamin <alaminprogramerr@gmail.com>
2020-09-17 11:15:41 -0700

    rivision 1

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,28 @@
-        let x=document.getElementById('navToggler')
-        x.onclick=()=>{
-            let navClass=document.getElementById('navbarCollapsInd')
-            if(navClass.style.height==="360px"){
-                navClass.style.height="100px"
-            }else{
-                navClass.style.height="360px"
-            }
-        }
+let x = document.getElementById('navToggler')
+let loginFormToggler = document.getElementById('login-form-toggler')
+let loginFormToggler2 = document.getElementById('login-form-toggler2')
+let loginForm = document.getElementById('login-form')
+
+loginFormToggler2.onclick=()=>{
+    if(loginForm.style.display==="block"){
+        loginForm.style.display="none"
+    }else{
+        loginForm.style.display="block"
+    }
+}
+loginFormToggler.onclick=()=>{
+    if(loginForm.style.display==="block"){
+        loginForm.style.display="none"
+    }else{
+        loginForm.style.display="block"
+    }
+}
+x.onclick = () => {
+    let navClass = document.getElementById('navbarCollapsInd')
+    if (navClass.style.height === "360px") {
+        navClass.style.height = "100px"
+    } else {
+        navClass.style.height = "360px"
+    }
+}
+
