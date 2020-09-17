45189bdfa6e05b3a43113d105a8d3a776a305190
Alamin <alaminprogramerr@gmail.com>
2020-09-17 15:09:00 -0700

    resolved6

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,57 @@ let x = document.getElementById('navToggler')
 let loginFormToggler = document.getElementById('login-form-toggler')
 let loginFormToggler2 = document.getElementById('login-form-toggler2')
 let loginForm = document.getElementById('login-form')
+let scrooler = document.getElementById('scrooler')
+let scrooler1 = document.getElementById('scrooler1')
+let scrooler2 = document.getElementById('scrooler2')
+let scrooler3 = document.getElementById('scrooler3')
+let scrooler4 = document.getElementById('scrooler4')
 
+scrooler4.onclick=()=>{
+    setTimeout(() => {
+        let p=window.scrollY
+        window.scrollTo({
+            top: p-100,
+            left: 0
+          });
+    }, 10);
+}
+scrooler3.onclick=()=>{
+    setTimeout(() => {
+        let p=window.scrollY
+        window.scrollTo({
+            top: p-100,
+            left: 0
+          });
+    }, 10);
+}
+scrooler2.onclick=()=>{
+    setTimeout(() => {
+        let p=window.scrollY
+        window.scrollTo({
+            top: p-100,
+            left: 0
+          });
+    }, 10);
+}
+scrooler1.onclick=()=>{
+    setTimeout(() => {
+        let p=window.scrollY
+        window.scrollTo({
+            top: p-100,
+            left: 0
+          });
+    }, 10);
+}
+scrooler.onclick=()=>{
+    setTimeout(() => {
+        let p=window.scrollY
+        window.scrollTo({
+            top: p-100,
+            left: 0
+          });
+    }, 100);
+}
 loginFormToggler2.onclick=()=>{
     if(loginForm.style.display==="block"){
         loginForm.style.display="none"
